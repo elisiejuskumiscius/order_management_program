c65c18cdf5d55cec0f1c0e10b07e98e9b38877dd
elisiejus <ekumiscius@gmail.com>
2020-12-03 23:41:44 +0200

    Initial commit

---
+++ Csharp_Login_And_Register/DB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Csharp_Login_And_Register
+{
+
+    class DB
+    {
+        public static String cs
+        {
+            get { return cs; }
+            set { cs = value; }
+        }
+
+        string value = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\order_management_program\Csharp_Login_And_Register\Database.mdf;Integrated Security=True";
+
+        SqlConnection connection = new SqlConnection(cs);
+
+        // create a function to open the connection
+        public void openConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        // create a function to close the connection
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        // create a function to return the connection
+        public SqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ Csharp_Login_And_Register/LoginForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_Login_And_Register
+{
+    public partial class LoginForm : Form
+    {
+        
+        public LoginForm()
+        {
+            InitializeComponent();
+
+            this.textBoxPassword.AutoSize = false;
+            this.textBoxPassword.Size = new Size(this.textBoxPassword.Size.Width, 50);
+        }
+
+        private void labelClose_MouseEnter(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.Black;
+        }
+
+        private void labelClose_MouseLeave(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.White;
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+        }
+        string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\order_management_program\Csharp_Login_And_Register\Database.mdf;Integrated Security=True";
+
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxUsername.Text == "" || textBoxPassword.Text == "")
+            {
+                MessageBox.Show("Please provide Username and Password");
+                return;
+            }
+
+            try
+            {
+                //Create SqlConnection
+                SqlConnection con = new SqlConnection(cs);
+                SqlCommand cmd = new SqlCommand("Select * from Users where Username=@username and Password=@password", con);
+                cmd.Parameters.AddWithValue("@username", textBoxUsername.Text);
+                cmd.Parameters.AddWithValue("@password", textBoxPassword.Text);
+                con.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                con.Close();
+                int count = ds.Tables[0].Rows.Count;
+                //If count is equal to 1, than show frmMain form
+                if (count == 1)
+                {
+                    MessageBox.Show("Login Successful!");
+                    this.Hide();
+                    MainForm mf = new MainForm();
+                    mf.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void labelGoToSignUp_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegisterForm registerform = new RegisterForm();
+            registerform.Show();
+        }
+
+        private void labelGoToSignUp_MouseEnter(object sender, EventArgs e)
+        {
+            labelGoToSignUp.ForeColor = Color.Yellow;
+        }
+
+        private void labelGoToSignUp_MouseLeave(object sender, EventArgs e)
+        {
+            labelGoToSignUp.ForeColor = Color.White;
+        }
+    }
+}
